2ef71c3725e59703caab7965bd27659b49fb22db
zrwinte <zrwinte@ilstu.edu>
2025-02-20 15:42:52 -0600

    Update LedControl.ino

--- LedControl.ino
+++ LedControl.ino
@@ -1,1 +1,8 @@
+//Code to control and RGB Led via arduino from a breadboard
+
+
+//pin 7 for red, pin 6 for green, pin 5 for blue RGB LED
+//red button = pin 4, green button = pin 3, blue button = pin 2
+
+
 
